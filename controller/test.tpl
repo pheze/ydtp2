e1739973ce327d102115b1e663dabe1f6ba37a2f
Dom <dom@pHzb0x>
2009-09-23 12:43:08 -0400

    needed to merge and manually delete the old Dwoo thing..

--- controller/test.tpl
+++
@@ -1,6 +0,0 @@
-<html>
-	<body>
-		<h1>{$a}</h1>
-		<p>{$b}</p>
-	</body>
-</html>
--- lib/dwoo/compiled/07875a25df087702c6db911959fc944f.d16.php
+++
@@ -1,4 +0,0 @@
-<?php
-ob_start(); /* template body */ ?>test.tpl<?php  /* end template body */
-return $this->buffer . ob_get_clean();
-?>
